40dc18dea1faa7edaff71520ee48e9c203452e8d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-17 00:55:38 +0300

    Remove global log(...)

--- Sources/Log/Globals.swift
+++
@@ -1,7 +0,0 @@
-public func log(
-    level: Log.Message.Level,
-    source: Log.Message.Source = .init(),
-    message: @autoclosure () -> String)
-{
-    Log.handle(level: level, source: source, message: message)
-}
